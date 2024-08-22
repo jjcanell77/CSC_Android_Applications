656d76e63998daef761301e134103c6bf912dfa6
Jesse James Canell Jr <119881707+jjcanell77@users.noreply.github.com>
2024-08-21 22:32:16 -0500

    Initial commit

---
+++ app/src/main/java/com/example/helloandroidapplication/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.helloandroidapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+// import androidx.activity.compose.setContent
+// import androidx.activity.enableEdgeToEdge
+// import androidx.compose.foundation.layout.fillMaxSize
+// import androidx.compose.foundation.layout.padding
+// import androidx.compose.material3.Scaffold
+// import androidx.compose.material3.Surface
+// import androidx.compose.material3.Text
+// import androidx.compose.runtime.Composable
+// import androidx.compose.ui.Modifier
+// import androidx.compose.ui.graphics.Color
+// import androidx.compose.ui.tooling.preview.Preview
+// import androidx.compose.ui.unit.dp
+// import com.example.helloandroidapplication.ui.theme.HelloAndroidApplicationTheme
+// import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        enableEdgeToEdge()
+//        setContent {
+//            HelloAndroidApplicationTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
+    }
+}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Surface( color = Color.Cyan) {
+//        Text(
+//            text = "Hello $name!",
+//            modifier = modifier.padding(24.dp)
+//        )
+//    }
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    HelloAndroidApplicationTheme {
+//        Greeting("Android")
+//    }
+//}
